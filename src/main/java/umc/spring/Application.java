85e1ed3eaf4dfc661630ee86bb2e8febc5761d66
hyouzl <hyouzl0521@gmail.com>
2024-12-26 14:28:15 +0900

    fix: test 설정 지우기

--- src/main/java/umc/spring/Application.java
+++ src/main/java/umc/spring/Application.java
@@ -16,22 +16,4 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@Bean
-	public CommandLineRunner run(ApplicationContext context) {
-		return args -> {
-			MemberQueryService memberQueryServiceQueryService = context.getBean(MemberQueryService.class);
-
-			// 파라미터 값 설정
-			Long memberId = 1L;
-
-
-			// 쿼리 메서드 호출 및 쿼리 문자열과 파라미터 출력
-			System.out.println("Executing findStoresByNameAndScore with parameters:");
-
-
-
-			memberQueryServiceQueryService.findMemberInMyPage(memberId);
-
-		};
-	}
 }
--- src/test/java/umc/spring/ApplicationTests.java
+++ src/test/java/umc/spring/ApplicationTests.java
@@ -3,11 +3,6 @@ package umc.spring;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
